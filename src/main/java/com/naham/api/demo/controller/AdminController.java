bf381f6a60d112dbe18022230937ad273cecdb0a
sizovn <sizovns.13@gmail.com>
2024-04-07 17:59:36 +0300

    Added httpd and path traversal possibility

--- src/main/java/com/naham/api/demo/controller/AdminController.java
+++ src/main/java/com/naham/api/demo/controller/AdminController.java
@@ -1,14 +1,27 @@
 package com.naham.api.demo.controller;
 
+import jakarta.websocket.server.PathParam;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Slf4j
 @RestController
 public class AdminController {
 
     @GetMapping("/admin")
-    public String admin() {
+    public String admin(@PathParam("id") Long id) {
+        return testAdmin();
+    }
+
+/*    @GetMapping("/hello/../admin")
+    public String admin2() {
+        return testAdmin();
+    }*/
+
+    private String testAdmin() {
+        log.info("Get admin info");
         return "Fucking shit!";
     }
+
 }
--- src/main/java/com/naham/api/demo/controller/TestController.java
+++ src/main/java/com/naham/api/demo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.naham.api.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,13 +8,22 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+@Slf4j
 @RestController
 public class TestController {
 
     @GetMapping("/hello")
     public Collection<String> sayHello() {
+        log.info("Hello endpoint!");
         return IntStream.range(0, 10)
                 .mapToObj(i -> "Hello number " + i)
                 .collect(Collectors.toList());
+    }
+
+
+    @GetMapping("/")
+    public String greetings() {
+        log.info("Greetings enpdoint on /");
+        return "Hello word!";
     }
 }
